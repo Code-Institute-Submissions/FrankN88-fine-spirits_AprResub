375bc6c6aa6224e91b8065a5b109ba18439b32dd
FrankN88 <nardecchia.f@gmail.com>
2022-01-09 21:30:39 +0000

    removed again secret key

--- fine_spirits/settings.py
+++ fine_spirits/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s6!g7-%ihm8#i8wpn8#ip!*a=@n7(qvf99@wpfl3ibe4h_-*md'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
