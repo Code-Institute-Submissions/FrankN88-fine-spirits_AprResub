7a92c75edc69808455d104e8dfd60d2c37f55f73
FrankN88 <nardecchia.f@gmail.com>
2022-01-15 13:07:14 +0100

    Added category to nav-bar with related functionality to filter products

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category
 
 # Create your views here.
 def all_products(request):
@@ -9,9 +9,15 @@ def all_products(request):
 	# A view of a list of all products
 
 	products = Product.objects.all()
+	categories = None
 	query = None
 
 	if request.GET:
+		if 'category' in request.GET:
+			categories = request.GET['category'].split(',')
+			products = products.filter(category__name__in=categories)
+			categories = Category.objects.filter(name__in=categories)
+
 		if 'q' in request.GET:
 			query = request.GET['q']
 			if not query:
@@ -24,6 +30,7 @@ def all_products(request):
 	context = {
 		'products': products,
 		'search_term': query,
+		'current_categories': categories,
 	}
 
 	return render(request, "products/products.html", context)
