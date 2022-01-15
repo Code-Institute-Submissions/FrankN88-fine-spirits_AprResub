4c1678ef9aaddc9bc6975dabcb02a5d52bfee3aa
FrankN88 <nardecchia.f@gmail.com>
2022-01-15 11:31:25 +0100

    Adding single product page with style and script in js for add or subtract quantity

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,17 @@
 function menu(){
 	$(".nav-bar-bottom").toggleClass("menu-open");
 }
+
+$(function(){
+	$(".quantity_minus").click(function(){
+		var quantity = parseInt($("[name='quantity']").val())-1;
+		if(quantity<1) return;
+		$("[name='quantity']").val(quantity);
+	});
+
+	$(".quantity_plus").click(function(){
+		var quantity = parseInt($("[name='quantity']").val())+1;
+		if(quantity>99) return;
+		$("[name='quantity']").val(quantity);
+	});
+});
