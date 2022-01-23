cb62474245ee4d278a9f8ad5ab31903305a7b988
FrankN88 <nardecchia.f@gmail.com>
2022-01-23 13:13:51 +0100

    Added button "scroll to top"

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,21 @@
 function menu(){
 	$(".nav-bar-bottom").toggleClass("menu-open");
 }
+
+/*
+	SCROLL TO TOP - ANIMATION + SCROLL
+*/
+$(window).scroll(function(){
+	var pos = window.scrollY;
+	if(pos > 50){
+		$(".scroll_to_top").show();
+	}else{
+		$(".scroll_to_top").hide();		
+	}
+});
+
+$(function(){
+	$(".scroll_to_top").click(function(){
+		$("html, body").animate({scrollTop:0}, 500);
+	});
+})	
