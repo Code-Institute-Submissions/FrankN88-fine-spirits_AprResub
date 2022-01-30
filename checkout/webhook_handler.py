fd34682d6138e0624b7b5137ee76b87e4802f778
FrankN88 <nardecchia.f@gmail.com>
2022-01-30 14:21:54 +0100

    Fix webhook handler data send

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -69,8 +69,7 @@ class StripeWH_Handler:
                 profile.default_country = shipping_details.address.country
                 profile.default_postcode = shipping_details.address.postal_code
                 profile.default_town_or_city = shipping_details.address.city
-                profile.default_street_address1 = shipping_details.address.line1
-                profile.default_street_address2 = shipping_details.address.line2
+                profile.default_street_address = shipping_details.address.line1
                 profile.default_county = shipping_details.address.state
                 profile.save()
 
@@ -126,15 +125,6 @@ class StripeWH_Handler:
                             quantity=item_data,
                         )
                         order_line_item.save()
-                    else:
-                        for size, quantity in item_data['items_by_size'].items():
-                            order_line_item = OrderLineItem(
-                                order=order,
-                                product=product,
-                                quantity=quantity,
-                                product_size=size,
-                            )
-                            order_line_item.save()
             except Exception as e:
                 if order:
                     order.delete()
