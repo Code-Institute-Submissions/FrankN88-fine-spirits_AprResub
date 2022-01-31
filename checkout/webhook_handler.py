026ff33f318a90c9ed2d7736285c963eeffc4aea
FrankN88 <nardecchia.f@gmail.com>
2022-01-31 10:24:00 +0000

    changed Debug to DEVELOPMENT in os.environ

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -19,6 +19,7 @@ class StripeWH_Handler:
     def _send_confirmation_email(self, order):
         """Send the user a confirmation email"""
         cust_email = order.email
+        print(cust_email)
         subject = render_to_string(
             'checkout/confirmation_emails/confirmation_email_subject.txt',
             {'order': order})
